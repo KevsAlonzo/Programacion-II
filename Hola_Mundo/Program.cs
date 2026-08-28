981961ff8ebc7879d548ce5f84dffb66c72c08dc
KevsAlonzo <kalonzob@miumg.edu.gt>
2026-08-28 00:49:41 -0600

    Agregar archivos de proyecto.

---
+++ Hola_Mundo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hola_Mundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola mundooo!");
+        }
+    }
+}
